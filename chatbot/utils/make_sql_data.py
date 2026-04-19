d0c7ced8993e28d332a635be20fdc3aa6e5f8421
Hasyim <hasyimagiel@gmail.com>
2026-04-19 22:48:29 +0700

    make sql data from csv

---
+++ chatbot/utils/make_sql_data.py
@@ -0,0 +1,55 @@
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column, Integer, String, Float, text
+from sqlalchemy import create_engine
+import pandas as pd
+
+data_path = "/home/hasyim/Bootcamp_AI/capston/Capston3/chatbot/data/raw/imdb_top_1000.csv"
+df = pd.read_csv(data_path)
+
+df=df.replace({'Released_Year': 'PG'}, None)
+
+df['Gross'] = df['Gross'].str.replace(',', '', regex=True)
+
+df_clean = df.drop(columns="Overview")
+
+df_clean[['Released_Year','Gross']] = df_clean[['Released_Year','Gross']].apply(pd.to_numeric)
+
+df_clean.index += 1
+df_clean.index.name = 'film_id'
+df_ke_sql = df_clean.reset_index()
+
+engine = create_engine('sqlite:////home/hasyim/Bootcamp_AI/capston/Capston3/chatbot/data/process/IMDB_FILM.db')
+
+with engine.begin() as conn:
+    conn.execute(text("DROP TABLE IF EXISTS FILM_TABEL"))
+    
+metadata_obj = MetaData()
+
+FILM = Table(
+    "FILM_TABEL",
+    metadata_obj,
+    Column("film_id", Integer, primary_key=True),
+    Column("Series_Title", String, nullable=False),
+    Column("Released_Year", Float),
+    Column("Certificate", String),
+    Column("Runtime", String),
+    Column("Genre", String),
+    Column("IMDB_Rating", Float),
+    Column("Meta_score", Float),
+    Column("Director", String),
+    Column("Star1", String),
+    Column("Star2", String),
+    Column("Star3", String),
+    Column("Star4", String),
+    Column("No_of_Votes", Integer),
+    Column("Gross", Float),
+    Column("Poster_Link", String),
+    extend_existing=True
+)
+
+df_ke_sql.to_sql(
+    name='FILM_TABEL',
+    con=engine,
+    if_exists='append',
+    index=False
+)
