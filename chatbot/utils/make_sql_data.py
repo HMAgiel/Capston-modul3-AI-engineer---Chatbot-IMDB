782fa266746edaa553a283faf5f5a6289e6a6eec
Hasyim <hasyimagiel@gmail.com>
2026-04-22 22:42:30 +0700

    make sql data fix

--- chatbot/utils/make_sql_data.py
+++ chatbot/utils/make_sql_data.py
@@ -2,6 +2,7 @@ from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String, Float, text
 from sqlalchemy import create_engine
 import pandas as pd
+from uuid import uuid4
 
 data_path = "/home/hasyim/Bootcamp_AI/capston/Capston3/chatbot/data/raw/imdb_top_1000.csv"
 df = pd.read_csv(data_path)
@@ -14,11 +15,9 @@ df_clean = df.drop(columns="Overview")
 
 df_clean[['Released_Year','Gross']] = df_clean[['Released_Year','Gross']].apply(pd.to_numeric)
 
-df_clean.index += 1
-df_clean.index.name = 'film_id'
-df_ke_sql = df_clean.reset_index()
+df_clean['film_id'] = [str(uuid4()) for _ in range(len(df_clean['Series_Title']))]
 
-engine = create_engine('sqlite:////home/hasyim/Bootcamp_AI/capston/Capston3/chatbot/data/process/IMDB_FILM.db')
+engine = create_engine('sqlite:////home/hasyim/Bootcamp_AI/capston/Capston3/chatbot/data/process/IMDB_FILM_capston3.db')
 
 with engine.begin() as conn:
     conn.execute(text("DROP TABLE IF EXISTS FILM_TABEL"))
@@ -28,7 +27,7 @@ metadata_obj = MetaData()
 FILM = Table(
     "FILM_TABEL",
     metadata_obj,
-    Column("film_id", Integer, primary_key=True),
+    Column("film_id", String, primary_key=True, default=lambda: str(uuid4())),
     Column("Series_Title", String, nullable=False),
     Column("Released_Year", Float),
     Column("Certificate", String),
@@ -47,7 +46,7 @@ FILM = Table(
     extend_existing=True
 )
 
-df_ke_sql.to_sql(
+df_clean.to_sql(
     name='FILM_TABEL',
     con=engine,
     if_exists='append',
